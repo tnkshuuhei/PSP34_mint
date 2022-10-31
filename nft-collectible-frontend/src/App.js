97128b07636d29ef098d29afbccdbd8c4bde2dd7
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2022-10-31 15:16:28 +0900

    fix some error

--- nft-collectible-frontend/src/App.js
+++ nft-collectible-frontend/src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
-import {Button, Container, Row, Col, Form, Collapse} from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { Button, Container, Row, Col, Form, Collapse } from "react-bootstrap";
 import {
     web3Accounts,
     web3Enable,
@@ -19,15 +14,17 @@ import { ContractPromise } from '@polkadot/api-contract';
 import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
 import ContractABI from "./psp34Contract.json";
 import React, { useEffect, useState } from "react";
-
 import * as S from "./style";
 import Globalmenu from './components/Globalmenu.js';
 import styled from "styled-components";
-//import Button from 'react-bootstrap/Button';
-//import Collapse from 'react-bootstrap/Collapse';
 
+const WS_PROVIDER = 'wss://shibuya.public.blastapi.io'
+const DAPP_NAME = 'Mint psp34NFT'
 
 function App() {
+    useEffect(() => { 
+        checkIfWalletIsConnected();
+    }, []); 
     const CONTRACT_ADDRESS = "YUXuMznMWLcKti9G7QTz29vsz5AtySSWEAuufnDoLAGC6ff";
     const [currentAccount, setCurrentAccount] = useState(null);
     const [allAccounts, setAllAccounts] = useState(null);
@@ -37,55 +34,54 @@ function App() {
     const [nftdescript, setNftdescript] = useState(null);
     const [nftimg, setNftimg] = useState(null);
     const [nftdate, setNftdate] = useState(null);
-    
-    const checkWalletIsConnected = (async () => {
-        const extensions = await web3Enable('psp34Contract');
-        
-        if (extensions.length === 0) {
-            // no extension installed, or the user did not accept the authorization
-            // in this case we should inform the use and give a link to the extension
-            console.log("no extension");
-            return;
-        }
-    });
 
-    const connectWalletHanlder = async() => {
-        const allAccounts = await web3Accounts();
-        setAllAccounts(allAccounts);
-        let unsubscribe; // this is the function of type `() => void` that should be called to unsubscribe
-
-        // we subscribe to any account change and log the new list.
-        // note that `web3AccountsSubscribe` returns the function to unsubscribe
-        if (allAccounts) {
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const extensions = await web3Enable(DAPP_NAME);
+            if (extensions.length === 0) {
+                // no extension installed, or the user did not accept the authorization
+                // in this case we should inform the use and give a link to the extension
+                return;
+            }
+            const allAccounts = await web3Accounts();
             setCurrentAccount(allAccounts[0]);
-            unsubscribe = await web3AccountsSubscribe(( allAccounts ) => { 
-                allAccounts.map(( account ) => {
-                    console.log("account address ", account.address);
-                })
-            });
-
-            unsubscribe && unsubscribe();
+        } catch (error) {
+            console.error(error)
         }
-    }
+    };
 
-    const mintNftHandler = async() => {
-        if ( allAccounts ) {
-            const wsProvider = new WsProvider("wss://rpc.shibuya.astar.network");
-            const api = await ApiPromise.create({ provider: wsProvider });
+    const connectWalletHanlder = async () => {
+        try {
+            const extensions = await web3Enable(DAPP_NAME);
+            if (extensions.length === 0) {
+                // no extension installed, or the user did not accept the authorization
+                // in this case we should inform the use and give a link to the extension
+                return;
+            }
+            const allAccounts = await web3Accounts();
+            setCurrentAccount(allAccounts[0]);
+        } catch (error) {
+            console.error(error)
+        }
+    };
+
+    const mintNftHandler = async () => {
+        if (allAccounts) {
+            const provider = new WsProvider(WS_PROVIDER);
+            const api = await ApiPromise.create({ provider });
             const psp34 = new ContractPromise(api, ContractABI, CONTRACT_ADDRESS);
 
             const gasLimit = -1;
-
-            const mintTokenExtrinsic = await psp34.tx.mintToken({gasLimit});
             const injector = await web3FromSource(currentAccount.meta.source);
-
+            const mintTokenExtrinsic = await psp34.tx.mintToken({ gasLimit });
+            
             mintTokenExtrinsic.signAndSend(currentAccount.address, { signer: injector.signer }, ({ status }) => {
                 if (status.isInBlock) {
                     console.log(`Completed at block hash #${status.asInBlock.toString()}`);
                 } else {
                     console.log(`Current status: ${status.type}`);
                 }
-            }).catch((error: any) => {
+            }).catch((error) => {
                 console.log(':( transaction failed', error);
             });
         }
@@ -93,49 +89,45 @@ function App() {
 
     const connectWalletButton = () => {
         return (
-          <button onClick={connectWalletHanlder} className='cta-button connect-wallet-button'>
-            Connect Wallet
-          </button>
+            <button onClick={connectWalletHanlder} className='cta-button connect-wallet-button'>
+                Connect Wallet
+            </button>
         )
-      }
-    
-      const mintNftButton = () => {
+    }
+
+    const mintNftButton = () => {
         return (
-          <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
-            Mint NFT
-          </button>
+            <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+                Mint NFT
+            </button>
         )
     }
 
     const setName = (e) => {
-        console.log("name: ",e);
         setNftname(e);
     }
 
     const setDescript = (e) => {
-        console.log("script: ",e);
         setNftdescript(e);
     }
 
     const setDate = (e) => {
-        console.log("script: ",e);
         setNftdate(e);
     }
 
     const setNftimage = (e) => {
-        console.log("image: ",e);
         setNftimg(e);
     }
 
     const collapseVisible = () => {
         return (
-                <S.Collapse>
-                    <Collapse in={currentAccount}>
-                        <S.Container_Mi>
+            <S.Collapse>
+                <Collapse in={currentAccount}>
+                    <S.Container_Mi>
                         <Form>
                             <Form.Group className="mb-3" controlId="mintForm.ControlInput1">
                                 <Form.Label>NFT Name</Form.Label>
-                                <Form.Control type="text" placeholder="Your NFT's name" onChange={e => setName(e.target.value)}/>
+                                <Form.Control type="text" placeholder="Your NFT's name" onChange={e => setName(e.target.value)} />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="mintForm.ControlTextarea1">
                                 <Form.Label>Description</Form.Label>
@@ -152,16 +144,13 @@ function App() {
                                 <Form.Control type="file" onChange={e => setNftimage(e.target.value)} />
                             </Form.Group>
                         </Form>
-                        </S.Container_Mi>
-                    </Collapse>
-                </S.Collapse>
-            
+                    </S.Container_Mi>
+                </Collapse>
+            </S.Collapse>
+
         )
     }
 
-    useEffect(() => { 
-        checkWalletIsConnected();
-    }, []);
 
     return (
         <div>
@@ -172,11 +161,11 @@ function App() {
                 <div className='main-app'>
                     <h1>PSP34 Contract</h1>
                     <div>
-                        { currentAccount ? mintNftButton() : connectWalletButton() }
+                        {currentAccount ? mintNftButton() : connectWalletButton()}
                     </div>
-                </div> 
+                </div>
                 <div>
-                    { currentAccount ? collapseVisible(): null}
+                    {currentAccount ? collapseVisible() : null}
                 </div>
             </BG_Container>
         </div>
